133a0b9276f7b0444b1b9bcbda018685f72e66eb
GharbiALI <gharbialicontacts@gmail.com>
2026-04-15 23:54:29 +0100

    chore: update routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from "express";
+import { validateUserMiddleware, validateIdMiddleware } from '../middleware/user.middleware';
 const router = Router();
 
 
+router.post('/', validateUserMiddleware, (req: Request, res: Response) => {
+    res.send('Welcome to the API!');
+});
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/:id', validateIdMiddleware, (req: Request, res: Response) => {
     res.send('Welcome to the API!');
 });
 
